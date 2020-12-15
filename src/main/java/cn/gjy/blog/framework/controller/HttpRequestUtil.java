b4c7c27974c6f8f4f9d77a496b02cf5e96d3799e
root <956115488@qq.com>
2020-12-15 23:33:55 +0800

    添加判断用户身份和获取用户ip

--- src/main/java/cn/gjy/blog/framework/controller/HttpRequestUtil.java
+++ src/main/java/cn/gjy/blog/framework/controller/HttpRequestUtil.java
@@ -1,5 +1,8 @@
 package cn.gjy.blog.framework.controller;
 
+import cn.gjy.blog.common.ContentString;
+import cn.gjy.blog.model.SysUser;
+
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -20,5 +23,25 @@ public class HttpRequestUtil {
 
     public static HttpServletRequest getRequest(){
         return requestThreadLocal.get();
+    }
+
+    public static String getRemoteIp(){
+       return getRequest().getRemoteAddr();
+    }
+
+    public static Integer getUserId(){
+        if(requestThreadLocal.get()!=null){
+            SysUser attribute = (SysUser) requestThreadLocal.get().getSession().getAttribute(ContentString.USER_SESSION_TAG);
+            if(attribute!=null)
+                return attribute.getId();
+        }
+        return null;
+    }
+
+    public static boolean isAdmin(){
+        if(requestThreadLocal.get()!=null){
+            return requestThreadLocal.get().getSession().getAttribute(ContentString.ADMIN_SESSION_TAG)!=null;
+        }
+        return false;
     }
 }
